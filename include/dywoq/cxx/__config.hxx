b9413bf449060adfb9670ffcee0b5f6e1148a38d
dywoq <aleks.koyf@gmail.com>
2025-12-29 14:17:05 +0300

    dywoq/cxx: Create __config,hxx file

---
+++ include/dywoq/cxx/__config.hxx
@@ -0,0 +1,12 @@
+#ifndef _DYWOQ_CXX___CONFIG_HXX
+#define _DYWOQ_CXX___CONFIG_HXX
+
+#ifndef __cplusplus
+#error "No C++ compiler detected"
+#endif
+
+#if !(__cplusplus >= 202002L)
+#error "Requires C++20 or higher"
+#endif
+
+#endif
