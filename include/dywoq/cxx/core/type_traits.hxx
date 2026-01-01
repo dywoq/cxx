acae8cc23fc6f0360d61d012203a6928673453b4
dywoq <aleks.koyf@gmail.com>
2026-01-01 15:07:26 +0300

    core: Add master header type_traits.hxx

---
+++ include/dywoq/cxx/core/type_traits.hxx
@@ -0,0 +1,19 @@
+/*
+ * dywoq 2025
+ *
+ * Licensed under Apache License 2.0:
+ * https://github.com/dywoq/cxx/blob/main/LICENSE
+ *
+ * Repository:
+ * https://github.com/dywoq/cxx
+ */
+#ifndef _DYWOQ_CXX_CORE_TYPE_TRAITS_HXX
+#define _DYWOQ_CXX_CORE_TYPE_TRAITS_HXX
+
+#include "../__config.hxx"
+
+#if _DYWOQ_CXX__VERSION_SUPPORTED
+
+#endif
+
+#endif
