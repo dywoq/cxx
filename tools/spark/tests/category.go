2fafa1aba774427aacd85f905371bf2240e31155
dywoq <aleks.koyf@gmail.com>
2026-01-02 11:37:55 +0300

    tools/spark: category: Rename CategoryConfig to Category

--- tools/spark/tests/category.go
+++ tools/spark/tests/category.go
@@ -1,6 +1,6 @@
 package test
 
-type CategoryConfig struct {
+type Category struct {
 	Name       string   `json:"name"`
 	TestFiles  []string `json:"test_files"`
 	Categories string   `json:"categories"`
