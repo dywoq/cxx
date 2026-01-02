82f84e50fb5d9536bbed170a6a62b23cb413e1b4
dywoq <aleks.koyf@gmail.com>
2026-01-02 16:13:38 +0300

    tools/spark: test: Remove Standard type and Standard parameter in Test structure
    Choosing standard is meaningless since dywoq/cxx will be supported on
    C++20 or higher

--- tools/spark/test/test.go
+++ tools/spark/test/test.go
@@ -6,20 +6,13 @@ import (
 	"os"
 )
 
-type Standard string
-
 type Type string
 
 type Test struct {
 	Name            string   `json:"name"`
-	Standard        Standard `json:"standard"`
 	AdditionalFlags []string `json:"additional_flags"`
 	Type            Type     `json:"type"`
 }
-
-const (
-	StandardCxx20 Standard = "cxx20"
-)
 
 const (
 	TypeCompilePass Type = "compile-pass"
