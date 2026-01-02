7a6cfe9e9ece6d39f4fe59f00cf5314ea9fa7dab
dywoq <aleks.koyf@gmail.com>
2026-01-02 15:34:22 +0300

    tools/spark: Add WithReader and WithFilepath in test

--- tools/spark/test/test.go
+++ tools/spark/test/test.go
@@ -1,6 +1,13 @@
 package test
 
+import (
+	"encoding/json"
+	"io"
+	"os"
+)
+
 type Standard string
+
 type Type string
 
 type Test struct {
@@ -9,6 +16,26 @@ type Test struct {
 	Standard        Standard `json:"standard"`
 	AdditionalFlags []string `json:"additional_flags"`
 	Type            Type     `json:"type"`
+}
+
+// WithReader populates the Test by decoding JSON data from r.
+// It expects r to contain a valid JSON object matching the Test schema.
+// Any existing fields in the Test may be overwritten.
+func (t *Test) WithReader(r io.Reader) error {
+	err := json.NewDecoder(r).Decode(t)
+	return err
+}
+
+// WithFilepath opens the file at path and populates the Test by decoding
+// its JSON contents. The file must contain a valid JSON object matching
+// the Test schema.
+func (t *Test) WithFilepath(path string) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return t.WithReader(f)
 }
 
 const (
